332e8bb17b28cc2259c37eacc7cb2c2772aaf9e0
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-04-05 15:42:43 -0300

    fix: user_id for userId

--- migrations/20210405182031-create-blog-posts.js
+++ migrations/20210405182031-create-blog-posts.js
@@ -17,7 +17,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
-      user_id: {
+      userId: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
