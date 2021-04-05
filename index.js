b1286a367a901c4c46c893a5961d5c4be591c95f
FlavioSugano <flavio.sugano@gmail.com>
2021-04-05 15:36:41 -0300

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -5,6 +6,7 @@ const app = express();
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
+app.get(
+  '/',
+  (_request, response) => response.send(),
+);
