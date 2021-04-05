0a284786bf0a1dafede65262dacad1e8de4b7128
isabellavjs <isabellavjs@gmail.com>
2021-04-05 17:40:15 -0300

    Cria as tabelas

---
+++ models/BlogPost.js
@@ -0,0 +1,20 @@
+const Post = (sequelize, DataTypes) => {
+  const blogPost = sequelize.define('Post', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  });
+
+  blogPost.associate = (models) => {
+    blogPost.belongsTo(models.Users,
+      { foreignKey: 'userId', as: 'user' });
+    return blogPost;
+  };
+
+  return blogPost;
+};
+
+module.exports = Post;
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const createUser = (sequelize, DataTypes) => {
+  const blogUser = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, { timestamps: false });
+
+  blogUser.associate = (models) => {
+    blogUser.hasMany(models.BlogPosts,
+      { foreignKey: 'userId', as: 'posts' });
+    return blogUser;
+  };
+
+  return blogUser;
+};
+
+module.exports = createUser;
