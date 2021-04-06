73e192bfab5478c0cd830df4bbd61aaf0639f6af
jederson <jmacedo.ti@gmail.com>
2021-04-06 16:57:39 -0300

    mudando rotas / arrumando lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const loginRouter = require('./controllers/loginController');
+// const loginRouter = require('./controllers/loginController');
 const userRouter = require('./controllers/userController');
 
 require('dotenv').config();
