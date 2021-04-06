cf49a6d0d04726e4dc66eb8bdd9a34d4294ce38a
ricardorosa-dev <ricardorosa@gmail.com>
2021-04-06 18:28:05 -0300

    Arrumando a variavel PORT

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use('/user', UsersController);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
