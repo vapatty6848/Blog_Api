07c8477036d10032d96df05f948ba0c7a27ae228
FlavioSugano <flavio.sugano@gmail.com>
2021-04-06 15:42:10 -0300

    build: installing body-parser, jwt, nodemon dependencies. Using .env variables on config.js file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const { json } = require('body-parser');
 
 const app = express();
+
+app.use(json());
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 // não remova esse endpoint, e para o avaliador funcionar
-app.get(
-  '/',
-  (_request, response) => response.send(),
-);
+app.get('/', (_request, response) => {
+  response.send();
+});
