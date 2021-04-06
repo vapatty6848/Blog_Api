3e82dc2ce77dc2e24bcde5a534f31567f4632c78
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-04-06 19:53:53 -0300

    test

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const loginController = require('./controllers/loginController');
 
 app.use(express.json());
 
-app.use('/user', UsersController);
-app.use('/login', loginController);
-
-app.get('/', (_req, res) => res.send('Hello World! o/'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/user', UsersController);
+app.use('/login', loginController);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
