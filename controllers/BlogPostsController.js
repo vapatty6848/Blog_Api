6930d0f3885ffefbf4bac1c541d7f509b8023aad
danieleperse <danieleperse3@gmail.com>
2021-04-06 19:41:56 -0300

    req. 8

--- controllers/BlogPostsController.js
+++ controllers/BlogPostsController.js
@@ -28,4 +28,18 @@ router.get('/post', validateToken, async (req, res) => {
   return res.status(200).json(posts);
 });
 
+router.get('/post/:id', validateToken, async (req, res) => {
+  const { id } = req.params;
+
+  const post = await BlogPosts.findByPk(id, {
+    attributes: { exclude: ['UserId'] },
+    include: [{
+      model: Users, as: 'user', attributes: { exclude: ['password'] },
+    }],
+  });
+
+  if (post) return res.status(200).json(post);
+  return res.status(404).json({ message: 'Post não existe' });
+});
+
 module.exports = router;
