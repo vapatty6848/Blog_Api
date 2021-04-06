33284c37a095ff7989b900c8dc8dfb1673057654
Tiago Berwanger <berwangertiago@gmail.com>
2021-04-06 14:22:47 -0300

    Requirement 1 - ajusting error message from README

--- middlewares/UserValidation.js
+++ middlewares/UserValidation.js
@@ -13,13 +13,17 @@ const UserValidation = (req, res, next) => {
   const { displayName, email, password } = req.body;
   const { error } = schema.validate({ displayName, email, password });
   try {
+    // Erro do README que obrigou criar uma mensagem incorreta personalizada.
+    if (error && error.details[0].message === '"password" length must be at least 6 characters long') {
+      return res.status(BAD_REQUEST).json({ message: '"password" length must be 6 characters long' });
+    }
     if (error) {
       return res.status(BAD_REQUEST).json({ message: error.details[0].message });
     }
+    next();
   } catch (err) {
     res.status(INTERNAL_ERROR).json({ message: err.message });
   }
-  next();
 };
 
 module.exports = UserValidation;
