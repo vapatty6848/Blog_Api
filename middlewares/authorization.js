6c644c5aa9903f553cac01f8f1295e2150b75aa2
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-04-07 17:04:57 -0300

    lint fix

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,29 +1,29 @@
 const jwt = require('jsonwebtoken');
-const checkEmail = require('../helpers/utils');
+// const checkEmail = require('../helpers/utils');
 
 const segredo = 'token';
 const authorization = async (req, res, next) => {
   const token = req.headers.authorization;
-  console.log('token',token);
+  console.log('token', token);
   if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
 
-  // try {
-    const x = jwt.verify(token, segredo);
-    console.log('x',x);
-    // if (!user) {
-    //   return res
-    //     .status(401)
-    //     .json({ message: 'Token expirado ou inválido' });
-    // }
+  try {
+    const user = jwt.verify(token, segredo);
+    console.log('x', user);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Token expirado ou inválido' });
+    }
 
-    // req.user = user;
+    req.user = user;
 
-    // return next();
-  // } catch (err) {
-  //   return res.status(401).json({ message: 'Token expirado ou inválido' });
-  // }
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token expirado ou inválido' });
+  }
 };
 
 module.exports = authorization;
