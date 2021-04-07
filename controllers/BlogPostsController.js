f5762a5700c85326f68f5e0e55c27c93827a1e40
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-04-07 18:05:42 -0300

    Requirement 10 push 01

--- controllers/BlogPostsController.js
+++ controllers/BlogPostsController.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { Op } = require('sequelize');
 const rescue = require('express-rescue');
 
 const { validateToken } = require('../middlewares/tokenValidation');
@@ -7,6 +8,26 @@ const { validateTitle, validateContent } = require('../middlewares/blogPostValid
 const BlogPostsController = Router();
 
 const { BlogPost, User } = require('../models');
+
+BlogPostsController.get('/search', validateToken, async (req, res) => {
+  const serachTerm = req.query;
+
+  const postBySearchTerm = await BlogPost.findAll({
+    where: {
+      [Op.or]: [
+        {
+          title: { [Op.like]: `%${serachTerm.q}%` },
+        },
+        {
+          content: { [Op.like]: `%${serachTerm.q}%` },
+        },
+      ],
+    },
+    include: { model: User, as: 'user' },
+  });
+
+  res.status(200).json(postBySearchTerm);
+});
 
 BlogPostsController.get('/', validateToken, async (req, res) => {
   const users = await BlogPost.findAll({
@@ -31,7 +52,7 @@ BlogPostsController.get('/:id', validateToken, async (req, res) => {
   res.status(200).json(postExists);
 });
 
-BlogPostsController.post('/', validateToken, validateTitle, validateContent, rescue(async (req, res) => {
+BlogPostsController.post('/post', validateToken, validateTitle, validateContent, rescue(async (req, res) => {
   const { title, content } = req.body;
   const userId = req.user.id;
   let published;
