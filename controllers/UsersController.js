d28f83e07a22e4fb2979574d90f0542b5b4c725c
Luciano Scalfone <luciano.scalfone@gmail.com>
2021-04-07 16:04:27 -0300

    trailing comma

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,7 +4,7 @@ const { validateUser } = require('../schemas/userValidation');
 const verifyAuth = require('../schemas/verifyAuth');
 const createToken = require('../auth/createToken');
 const {
-  OK, CONFLICT, SUCCESS, INTERNAL_SERVER_ERROR
+  OK, CONFLICT, SUCCESS, INTERNAL_SERVER_ERROR,
 } = require('../document/HTTPStatus');
 
 const router = new Router();
