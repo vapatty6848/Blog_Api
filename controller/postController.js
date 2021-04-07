783393da65a53ec31be48d8b1b3af153d890555f
Isa Koga <isadorakoga710@gmail.com>
2021-04-07 19:31:43 -0300

    :memo: Last req done, 10th dont pass

--- controller/postController.js
+++ controller/postController.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { Op } = require('sequelize');
 const { validateToken } = require('../middlewares/auth');
 const { validatePost } = require('../middlewares/validatePost');
 const { User, BlogPosts } = require('../models');
@@ -50,6 +51,40 @@ PostRouter.put('/:id', validateToken, validatePost, async (req, res) => {
   editedPost.content = content;
   await editedPost.save();
   return res.status(200).json({ title, content, userId });
+});
+
+PostRouter.get('/search', validateToken, async (req, res) => {
+  const { q } = req.query;
+
+  const posts = await BlogPosts.findAll({
+    where: {
+      [Op.or]: [{
+        title: {
+          [Op.like]: `%${q}%`,
+        },
+      },
+      {
+        content: {
+          [Op.like]: `%${q}%`,
+        },
+      }],
+    },
+    attributes: { exclude: 'userId' },
+    include: { model: User, as: 'user', attributes: { exclude: 'password' } },
+  });
+  if (!posts) return res.status(404).json({ message: 'Post não existe' });
+  return res.status(200).json(posts);
+});
+
+PostRouter.delete('/:id', validateToken, async (req, res) => {
+  const { id: userId } = req.payload.data;
+  const { id } = req.params;
+
+  const post = await BlogPosts.findOne({ where: { id } });
+  if (!post) return res.status(404).json({ message: 'Post não existe' });
+  if (post.userId !== userId) return res.status(401).json({ message: 'Usuário não autorizado' });
+  await BlogPosts.destroy({ where: { id } });
+  return res.status(204).send();
 });
 
 module.exports = { PostRouter };
