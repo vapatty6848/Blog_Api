77fbbb61cd70e67d28fea375b8a6887e2a412752
Isa Koga <isadorakoga710@gmail.com>
2021-04-07 18:56:16 -0300

    :memo: req Edit

--- controller/postController.js
+++ controller/postController.js
@@ -39,4 +39,17 @@ PostRouter.get('/:id', validateToken, async (req, res) => {
   return res.status(200).json(posts);
 });
 
+PostRouter.put('/:id', validateToken, validatePost, async (req, res) => {
+  const { title, content } = req.body;
+  const { id: userId } = req.payload.data;
+  const { id } = req.params;
+
+  const editedPost = await BlogPosts.findOne({ where: { id } });
+  if (editedPost.userId !== userId) return res.status(401).json({ message: 'Usuário não autorizado' });
+  editedPost.title = title;
+  editedPost.content = content;
+  await editedPost.save();
+  return res.status(200).json({ title, content, userId });
+});
+
 module.exports = { PostRouter };
