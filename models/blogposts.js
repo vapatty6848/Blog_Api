0bde5964672d059c4b396f0dfe96a27175da36c8
jederson <jmacedo.ti@gmail.com>
2021-04-07 08:37:38 -0300

    Requisito 7

--- models/blogposts.js
+++ models/blogposts.js
@@ -11,7 +11,7 @@ const BlogPosts = (sequelize, DataTypes) => {
   });
 
   blogposts.associate = (model) => {
-    blogposts.belongsTo(model.User, { foreignKey: 'userId', as: 'blogposts' });
+    blogposts.belongsTo(model.User, { foreignKey: 'userId', as: 'user' });
   };
 
   return blogposts;
