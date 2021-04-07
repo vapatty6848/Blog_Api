f28f03c2a51bf06bef8a5feb31e63e77426027a9
FlavioSugano <flavio.sugano@gmail.com>
2021-04-07 19:40:12 -0300

    creating statusCode and errorMessage files

---
+++ dictionary/errorMessage.js
@@ -0,0 +1,8 @@
+module.exports = {
+  INVALID_DISPLAYNAME: { message: '"displayName" length must be at least 8 characters long' },
+  INVALID_EMAIL: { message: '"email" must be a valid email' },
+  INVALID_PASSWORD: { message: '"password" length must be 6 characters long' },
+  EMAIL_REQUIRED: { message: '"email" is required' },
+  PASSWORD_REQUIRED: { message: '"password" is required' },
+  USER_EXISTS: { message: 'Usuário já existe' },
+};
