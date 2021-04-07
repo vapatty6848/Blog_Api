afa5d705cf517734762a36f77d4e8d70753dff70
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-04-07 09:53:21 -0300

    test3 (vamos evaluator plz)

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 const UsersController = require('./controllers/UsersController');
 const loginController = require('./controllers/loginController');
 
 app.use(express.json());
 
+app.use('/user', UsersController);
+app.use('/login', loginController);
+
+app.listen(PORT, () => console.log('ouvindo porta 3000!'));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
-
-app.use('/user', UsersController);
-app.use('/login', loginController);
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
