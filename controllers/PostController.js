293b65431690a3463b1beed4dc1b90e1b5c6a170
Luiz Mello <luizmacmillan@gmail.com>
2021-04-08 20:14:27 -0300

    Requisito 11.

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -9,6 +9,7 @@ const { decodeToken } = require('../services/DecodeToken');
 const PostController = new Router();
 const SUCCESS = 200;
 const CREATED = 201;
+const NO_CONTENT = 204;
 const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
@@ -60,33 +61,53 @@ PostController.put('/:id', validateToken, validatePost, async (req, res) => {
   const [email] = decodeToken(authorization);
   const validatedUserId = await User.findOne({ where: { email } })
     .then(({ id: foundId }) => foundId).catch((error) => unexpectedError(error, res));
+  const postToUpdate = BlogPost.findOne({ where: { id } }).then((post) => post)
+    .catch((error) => unexpectedError(error, res));
 
-  BlogPost.findOne({ where: { id } }).then((post) => {
-    if (post.userId !== validatedUserId) {
-      const message = 'Usuário não autorizado';
+  if (postToUpdate.userId !== validatedUserId) {
+    const message = 'Usuário não autorizado';
 
-      return res.status(UNAUTHORIZED).json({ message });
-    }
-    return BlogPost.update({ title, content }, { where: { id } });
-  }).then(() => res
-    .status(SUCCESS).json({ title, content, userId: validatedUserId }));
+    return res.status(UNAUTHORIZED).json({ message });
+  }
+  BlogPost.update({ title, content }, { where: { id } })
+    .then((_updatedPost) => res.status(SUCCESS).json({ title, content, userId: validatedUserId }))
+    .catch((error) => unexpectedError(error, res));
 });
 
-PostController.post('/', validateToken, validatePost, (req, res) => {
+PostController.post('/', validateToken, validatePost, async (req, res) => {
   const { headers: { authorization }, body: { title, content } } = req;
   const [email] = decodeToken(authorization);
+  const validatedUserId = await User.findOne({ where: { email } }).then((user) => user)
+    .catch((error) => unexpectedError(error, res));
 
-  User.findOne({ where: { email } }).then((user) => {
-    if (user !== null) {
-      const { id: userId } = user;
+  if (validatedUserId !== null) {
+    const { id: userId } = validatedUserId;
 
-      return BlogPost.create({ title, content, userId });
-    }
-  }).then(({
-    title: newTitle,
-    content: newContent,
-    userId,
-  }) => res.status(CREATED).json({ title: newTitle, content: newContent, userId }))
+    BlogPost.create({ title, content, userId })
+      .then((_createdPost) => res.status(CREATED).json({ title, content, userId }))
+      .catch((error) => unexpectedError(error, res));
+  }
+});
+
+PostController.delete('/:id', validateToken, async (req, res) => {
+  const { params: { id }, headers: { authorization } } = req;
+  const [email] = decodeToken(authorization);
+  const validatedUserId = await User.findOne({ where: { email } })
+    .then(({ id: foundId }) => foundId).catch((error) => unexpectedError(error, res));
+  const postToDelete = await BlogPost.findOne({ where: { id } }).then((post) => post)
+    .catch((error) => unexpectedError(error, res));
+
+  if (postToDelete === null) {
+    const message = 'Post não existe';
+
+    return res.status(NOT_FOUND).json({ message });
+  }
+  if (postToDelete.userId !== validatedUserId) {
+    const message = 'Usuário não autorizado';
+
+    return res.status(UNAUTHORIZED).json({ message });
+  }
+  BlogPost.destroy({ where: { id } }).then((_deletedPost) => res.status(NO_CONTENT).send())
     .catch((error) => unexpectedError(error, res));
 });
 
