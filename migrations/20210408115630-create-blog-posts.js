6ac6f1cdca161532aaf7f55cdb749a3c2f93930e
Usuario <mguimaraes.carneiro@gmail.com>
2021-04-08 10:25:05 -0300

    Blogs API Project | Requirement 02

--- migrations/20210408115630-create-blog-posts.js
+++ migrations/20210408115630-create-blog-posts.js
@@ -25,11 +25,13 @@ module.exports = {
       },
       published: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       },
       updated: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       }
     });
   },
