bc39a001e54d96e2042ed8371fbe40e57ce6aa96
Luiz Mello <luizmacmillan@gmail.com>
2021-04-08 15:22:56 -0300

    Requisito 4.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,15 +5,32 @@ const { generateToken } = require('../services/GenerateToken');
 const UserController = new Router();
 const SUCCESS = 200;
 const CREATED = 201;
+const NOT_FOUND = 404;
 const INTERNAL_SERVER_ERROR = 500;
-const message = 'Unexpected Error!';
+
+const unexpectedError = (error, res) => {
+  const message = 'Unexpected Error!';
+
+  console.log(error);
+  return res.status(INTERNAL_SERVER_ERROR).json({ message });
+};
 
 UserController.get('/', (_req, res) => {
   User.findAll().then((users) => res.status(SUCCESS).json(users))
-    .catch((error) => {
-      console.log(error);
-      return res.status(INTERNAL_SERVER_ERROR).json({ message });
-    });
+    .catch((error) => unexpectedError(error, res));
+});
+
+UserController.get('/:id', (req, res) => {
+  const { id } = req.params;
+  User.findOne({ where: { id } }).then((user) => {
+    if (user === null) {
+      const message = 'Usuário não existe';
+
+      return res.status(NOT_FOUND).json({ message });
+    }
+    return res.status(SUCCESS).json(user);
+  })
+    .catch((error) => unexpectedError(error, res));
 });
 
 UserController.post('/', (req, res) => {
@@ -22,10 +39,7 @@ UserController.post('/', (req, res) => {
 
   User.create({ displayName, email, password, image })
     .then((_user) => res.status(CREATED).json({ token }))
-    .catch((error) => {
-      console.log(error);
-      return res.status(INTERNAL_SERVER_ERROR).json({ message });
-    });
+    .catch((error) => unexpectedError(error, res));
 });
 
 module.exports = UserController;
