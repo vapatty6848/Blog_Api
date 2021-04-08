7f86e64fc25531e294e44d28f6eacac31251b504
Usuario <mguimaraes.carneiro@gmail.com>
2021-04-08 09:20:29 -0300

    Blogs API Project | Requirement 01 | FIX

--- middlewares/validateLogin.js
+++ middlewares/validateLogin.js
@@ -1,24 +1,24 @@
 const validateLogin = (req, res, next) => {
-    const { email, password } = req.body;
-    let message;
-    if (email === '') {
-      message = '"email" is not allowed to be empty';
-      return res.status(400).json({ message });
-    }
-    if (!email) {
-      message = '"email" is required';
-      return res.status(400).json({ message });
-    }
-    if (password === '') {
-      message = '"password" is not allowed to be empty';
-      return res.status(400).json({ message });
-    }
-    if (!password) {
-      message = '"password" is required';
-      return res.status(400).json({ message });
-    }
-    return next();
-  };
-  
-  module.exports = { validateLogin };
+  const { email, password } = req.body;
+  let message;
+  if (email === '') {
+    message = '"email" is not allowed to be empty';
+    return res.status(400).json({ message });
+  }
+  if (!email) {
+    message = '"email" is required';
+    return res.status(400).json({ message });
+  }
+  if (password === '') {
+    message = '"password" is not allowed to be empty';
+    return res.status(400).json({ message });
+  }
+  if (!password) {
+    message = '"password" is required';
+    return res.status(400).json({ message });
+  }
+  return next();
+};
+
+module.exports = { validateLogin };
   
