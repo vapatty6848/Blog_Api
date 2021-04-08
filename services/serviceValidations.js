2e393fd4bbec404453bb97e5a7d895e0acd81b43
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-04-08 17:47:57 -0300

    .

--- services/serviceValidations.js
+++ services/serviceValidations.js
@@ -5,7 +5,7 @@ const validateEmail = (email) => {
 const validateSignUp = (req, res, next) => {
   const { displayName, email, password } = req.body;
   if (displayName.length < 8) return res.status(400).json({ message: '"displayName" length must be at least 8 characters long' });
-  if (!email) res.status(400).json({ message: '"email" is required' });
+  if (!email) return res.status(400).json({ message: '"email" is required' });
   if (!validateEmail(email)) return res.status(400).json({ message: '"email" must be a valid email' });
   if (!password) return res.status(400).json({ message: '"password" is required' });
   if (password.length < 6) return res.status(400).json({ message: '"password" length must be 6 characters long' });
