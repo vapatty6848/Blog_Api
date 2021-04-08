721576b89eed8664b2d6c9ceab9f10ab68a5823b
FlavioSugano <flavio.sugano@gmail.com>
2021-04-08 00:10:58 -0300

    feat: adding login route for requirement 2

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const { json } = require('body-parser');
 const usersController = require('./controllers/users');
+const loginController = require('./controllers/login');
 
 const app = express();
 
 app.use(json());
 
 app.use('/user', usersController);
+app.use('/login', loginController);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => response.send());
