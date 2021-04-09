fc68a475fc29bb16aa91f2269c43b7d30047ad84
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-04-09 17:46:07 -0300

    .

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { validadeToken, validatePosts } = require('../services/serviceValidations');
-const { BlogPosts } = require('../models');
+const { User, BlogPosts } = require('../models');
 
 const postRouter = Router();
 
@@ -9,6 +9,34 @@ postRouter.post('/', validadeToken, validatePosts, async (req, res) => {
   const { title, content } = req.body;
   const post = await BlogPosts.create({ title, content, userId: id });
   return res.status(201).json(post);
+});
+
+postRouter.get('/', validadeToken, async (req, res) => {
+  const { id } = req.user.data;
+  const allPosts = await BlogPosts.findAll({
+    where: { userId: id },
+    attributes: { exclude: 'userId' },
+    include: { model: User, as: 'user', attributes: { exclude: 'password' } },
+  });
+  if (!allPosts) return res.status(404).json({ message: 'Post não existe' });
+  // console.log(`blogPost ${BlogPosts}`);
+  // console.log(`allPosts ${allPosts}`);
+  return res.status(200).json(allPosts);
+});
+
+postRouter.put('/:id', validadeToken, validatePosts, async (request, response) => {
+  const { title, content } = request.body;
+  const { id: userId } = request.user.data;
+  const { id } = request.params;
+
+  const update = await BlogPosts.findOne({ where: { id } });
+  if (update.userId !== userId) return response.status(401).json({ message: 'Usuário não autorizado' });
+
+  await BlogPosts.update({ title, content }, { where: { id } });
+
+  const postUpdated = await BlogPosts.findOne({ where: { id } });
+
+  return response.status(200).json(postUpdated);
 });
 
 module.exports = postRouter;
