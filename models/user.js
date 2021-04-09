e34d0ece7fcf5b0b47727d517102ca30390e9cba
Kesley Lahoz <namielahoz@gmail.com>
2021-04-09 15:45:24 -0300

    user model created

---
+++ models/user.js
@@ -0,0 +1,42 @@
+module.exports = (Sequelize, DataTypes) => {
+  const Users = Sequelize.define('Users', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  Users.associate = (models) => {
+    Users.hasMany(models.BlogPosts,
+      { foreignKey: 'userId', as: 'blogposts' });
+  };
+
+  return Users;
+};
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class User extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   User.init({
+//     displayName: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'User',
+//   });
+//   return User;
+// };
