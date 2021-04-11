bdebbc43847320a40d9a690fbed24c7a5cf1a38f
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-04-11 17:35:19 -0300

    :busts_in_silhouette: cria a migration para tabela de usuários

--- migrations/20210411125248-creates-user-table.js
+++ migrations/20210411125248-creates-user-table.js
@@ -1,21 +1,45 @@
 'use strict';
-
+// para validações : 
+// [https://sequelize.org/master/manual/validations-and-constraints.html]
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+   return queryInterface.createTable('Users', {
+     id: {
+       type: Sequelize.INTEGER,
+       primaryKey: true,
+       autoIncrement: true,
+       allowNull: false,
+     },
+     displayName: {
+       type: Sequelize.STRING,
+       allowNull: false, 
+       validate: {
+         min: 8
+       }
+     },
+     email: {
+       type: Sequelize.STRING,
+       allowNull: false,
+       validate: {
+         isEmail: true
+       }
+     },
+     password: {
+       type: Sequelize.STRING,
+       allowNull: false,
+       validate: {
+         min: 6,
+         max: 6
+       }
+     },
+     image: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    }
+   })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('Users');
   }
 };
