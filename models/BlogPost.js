7fccb9f22ffb2dafe0d2693f1e7150874e128250
Kesley Lahoz <namielahoz@gmail.com>
2021-04-11 12:48:54 -0300

    creating models

---
+++ models/BlogPost.js
@@ -0,0 +1,44 @@
+const { Sequelize } = require('sequelize');
+
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPosts = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: Sequelize.DATE,
+    updated: Sequelize.DATE,
+  });
+
+  BlogPosts.associate = (models) => {
+    BlogPosts.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPosts;
+};
+
+module.exports = BlogPost;
+
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class BlogPost extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   BlogPost.init({
+//     title: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'BlogPost',
+//   });
+//   return BlogPost;
+// };
