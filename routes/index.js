214be7a423c2ad11eab9768c59bd8de815c1f9d3
EliasCarvalho20 <eliascarvalho01@hotmail.com>
2021-04-12 13:13:47 -0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const { Router } = require('express');
 
 const usersRoutes = require('./usersRoutes');
 const loginRoutes = require('./loginRoutes');
-const postsRoutes = require('./postsRoutes');
+// const postsRoutes = require('./postsRoutes');
 
 const routes = Router();
 
 routes.use('/login', loginRoutes);
 routes.use('/user', usersRoutes);
-routes.use('/post', postsRoutes);
+// routes.use('/post', postsRoutes);
 
 module.exports = routes;
