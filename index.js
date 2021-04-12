c36e653d97492448958896ffb141f8b574e18534
Micaeli Teixeira <luime6@gmail.com>
2021-04-12 19:47:12 -0300

    refactor: add routes code

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const routes = require('./main.routes');
+
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(routes);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
