7eda9a5c4443ba08be29f03719b07a094ba4ca60
Micaeli Teixeira <luime6@gmail.com>
2021-04-12 19:44:51 -0300

    feat: using joi for validates

---
+++ schemas/index.js
@@ -0,0 +1,28 @@
+const Joi = require('joi');
+
+const sessionSchema = Joi.object({
+  email: Joi.string().email({ tlds: { allow: true } }).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const userSchema = Joi.object({
+  displayName: Joi.string().min(8),
+  email: Joi.string().email({ tlds: { allow: true } }).required(),
+  password: Joi.string().min(6).required().messages({
+    'string.base': '"password" should be a type of \'text\'',
+    'string.empty': '"password" cannot be an empty field',
+    'string.min': '"password" length must be {#limit} characters long',
+    'any.required': '"password" is required',
+  }),
+});
+
+const postSchema = Joi.object({
+  title: Joi.string().min(1).required(),
+  content: Joi.string().min(1).required(),
+});
+
+module.exports = {
+  sessionSchema,
+  userSchema,
+  postSchema,
+};
