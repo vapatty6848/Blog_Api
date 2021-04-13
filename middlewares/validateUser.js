e1ddbba5ef9ebb830f8b75f8bc82a7230a2be5f1
Jean Machado <jeanvictor.arq@gmail.com>
2021-04-13 20:11:17 -0300

    req 1

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -2,45 +2,49 @@ const { User } = require('../models');
 
 const { CONFLICT, BAD_REQUEST } = require('../errors/status');
 
-const validateEmail = (email, res) => {
+const validateEmail = (email) => {
   if (!email || email.length === 0) {
-    return res.status(400).json({ message: '"email" is required' });
+    return 'required';
   }
 
   const emailRegex = /\w+@(\w+\.)+\w+$/i;
   const emailIsValid = emailRegex.test(email);
 
   if (!emailIsValid) {
-    return res.status(BAD_REQUEST).json({ message: '"email" must be a valid email' });
+    return 'valid';
   }
 };
 
-const validatePassword = (password, res) => {
+const validatePassword = (password) => {
   if (!password) {
-    return res.status(BAD_REQUEST).json({ message: '"password" is required' });
+    return 'required';
   }
   if (password && password.length < 6) {
-    return res.status(BAD_REQUEST).json({ message: '"password" length must be 6 characters long' });
+    return 'minLength';
   }
 };
 
-const validateName = (displayName, res) => {
+const validateName = (displayName) => {
   if (displayName && displayName.length < 8) {
-    return res.status(BAD_REQUEST).json({ message: '"displayName" length must be at least 8 characters long' });
+    return 'minLength';
   }
 };
 
 module.exports = async (req, res, next) => {
   const { displayName, email, password } = req.body;
 
-  validateEmail(email, res);
+  const emailValdid = validateEmail(email, res);
+  if (emailValdid === 'required') return res.status(400).json({ message: '"email" is required' });
+  if (emailValdid === 'valid') return res.status(BAD_REQUEST).json({ message: '"email" must be a valid email' });
 
-  validatePassword(password, res);
+  const passwordValid = validatePassword(password, res);
+  if (passwordValid === 'required') return res.status(BAD_REQUEST).json({ message: '"password" is required' });
+  if (passwordValid === 'minLength') return res.status(BAD_REQUEST).json({ message: '"password" length must be 6 characters long' });
 
-  validateName(displayName, res);
+  const nameValid = validateName(displayName, res);
+  if (nameValid === 'minLength') return res.status(BAD_REQUEST).json({ message: '"displayName" length must be at least 8 characters long' });
 
   const userByEmail = await User.findOne({ where: { email } });
-
   if (userByEmail) {
     return res.status(CONFLICT).json({ message: 'Usuário já existe' });
   }
