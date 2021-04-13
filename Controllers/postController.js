c7570c8bbc378ff50708eb99f0d09b7293d2ac86
RenatoCostaDev <renatotrybe@gmail.com>
2021-04-13 14:56:25 -0300

    feat:Requisito-8

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -17,12 +17,22 @@ router.post('/', verifyAuthorization, validateTitleEntries,
 
 router.get('/', verifyAuthorization, async (req, res) => {
   const postArray = await BlogPosts.findAll({
-    include: [{
-      model: Users, as: 'user', attributes: { exclude: ['password'] },
-    }],
+    include: [{ model: Users, as: 'user', attributes: { exclude: ['password'] } }],
     attributes: { exclude: ['userId'] },
   });
-  console.log('array', postArray);
+
   return res.status(200).json(postArray);
 });
+
+router.get('/:id', verifyAuthorization, async (req, res) => {
+  const { id } = req.params;
+  const post = await BlogPosts.findOne({
+    where: { id },
+    include: { model: Users, as: 'user' },
+  });
+  if (!post) res.status(404).json({ message: 'Post não existe' });
+
+  res.status(200).json(post);
+});
+
 module.exports = router;
