5f7722e8f4302da61911064837116c1b7e69bf44
RenatoCostaDev <renatotrybe@gmail.com>
2021-04-12 22:14:05 -0300

    feat:Requisito-5

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 const { Users } = require('../models');
 const createToken = require('../Auth/createToken');
 const verifyAuthorization = require('../Auth/verifyAuthorization');
 const { validateDisplaynameEntries, newEmail, validatePasswordEntries, validateEmailEntries } = require('../Middlewares/userValidations');
+
+const mySecretKey = 'Hey-Ho!';
 
 const router = Router();
 router.post('/', validateDisplaynameEntries, validatePasswordEntries, validateEmailEntries,
@@ -24,12 +27,20 @@ router.get('/', verifyAuthorization, async (req, res) => {
 router.get('/:id', verifyAuthorization, async (req, res) => {
   const { id } = req.params;
   const userById = await Users.findByPk(id);
-  console.log(userById);
   if (userById) return res.status(200).json(userById);
   return res.status(404).json({ message: 'Usuário não existe' });
 });
 
-// router.delete('/me', verifyAuthorization, async (req, res) => {
-
-// })
+router.delete('/me', verifyAuthorization, (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const user = jwt.verify(token, mySecretKey);
+    console.log('user', user);
+    const { email } = user;
+    Users.destroy({ where: { email } });
+    return res.send(204);
+  } catch (e) {
+    return res.status(500).json({ message: e.message });
+  }
+});
 module.exports = router;
