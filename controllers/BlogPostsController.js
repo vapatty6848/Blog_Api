518c6b1ef8e0d3045bbb7d0f89532ca896e87edc
vapatty6848 <vapatty@gmail.com>
2021-04-13 19:13:53 -0300

    requisito 9 e 11

--- controllers/BlogPostsController.js
+++ controllers/BlogPostsController.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const AuthorizationUsers = require('../middlewares/authenticates');
 const validatedBlogPosts = require('../middlewares/validatedPosts');
-const { createNewPost, listPosts, listPostsId } = require('../services/PostsServices');
+const { createNewPost, listPosts, listPostsId, editPosts, postDelete } = require('../services/PostsServices');
 
 const BlogPostsController = new Router();
 
@@ -19,7 +19,8 @@ BlogPostsController.post('/', validatedBlogPosts, AuthorizationUsers, async (req
 
 BlogPostsController.get('/:id', AuthorizationUsers, async (req, res) => {
   const { id } = req.params;
-  const [post] = await listPostsId(id);
+  const userId = req.user;
+  const [post] = await listPostsId(id, userId);
   if (!post) return res.status(404).json({ message: 'Post não existe' });
   return res.status(200).json(post);
 });
@@ -27,6 +28,31 @@ BlogPostsController.get('/:id', AuthorizationUsers, async (req, res) => {
 BlogPostsController.get('/', AuthorizationUsers, async (req, res) => {
   const post = await listPosts();
   return res.status(200).json(post);
+});
+
+BlogPostsController.put('/:id', validatedBlogPosts, AuthorizationUsers, async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.user;
+  const { title, content } = req.body;
+  const post = await editPosts(id, title, content, userId);
+  console.log('put\n\n', post, userId);
+  // if (!post) return res.status(404).json({ message: 'Post não existe' });
+  if (!post) return res.status(401).json({ message: 'Usuário não autorizado' });
+  return res.status(200).json({
+    title,
+    content,
+    userId,
+  });
+});
+
+BlogPostsController.delete('/:id', AuthorizationUsers, async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.user;
+  const [post] = await listPostsId(id, userId);
+  if (!post) return res.status(404).json({ message: 'Post não existe' });
+  const postDel = await postDelete(id, userId);
+  if (!postDel) return res.status(401).json({ message: 'Usuário não autorizado' });
+  return res.status(204).json();
 });
 
 module.exports = BlogPostsController;
--- services/PostsServices.js
+++ services/PostsServices.js
@@ -1,3 +1,4 @@
+// const { Op } = require('sequelize');
 const { BlogPosts } = require('../models');
 
 const createNewPost = async (title, content, userId) => {
@@ -9,6 +10,7 @@ const createNewPost = async (title, content, userId) => {
     updated: new Date(),
   });
 };
+
 const listPostsId = async (id) => {
   const post = await BlogPosts.findAll({
     where: {
@@ -35,8 +37,46 @@ const listPosts = async () => {
   });
   return post;
 };
+
+const editPosts = async (id, title, content, userId) => {
+  try {
+    const findPost = await BlogPosts.findOne({
+      where: {
+        id,
+        userId,
+      },
+    });
+    if (!findPost) return null;
+
+    await BlogPosts.update(
+      {
+        title,
+        content,
+      },
+      { where: { id } },
+    );
+
+    return (title, content, userId);
+  } catch (error) {
+    console.log('erro interno edit', error.message);
+  }
+};
+
+const postDelete = async (id, userId) => {
+  try {
+    const postDeleted = await BlogPosts.destroy({
+      where: { id, userId },
+    });
+    return postDeleted;
+  } catch (error) {
+    console.log('erro interno delete', error.message);
+  }
+};
+
 module.exports = {
   createNewPost,
   listPosts,
   listPostsId,
+  editPosts,
+  postDelete,
 };
