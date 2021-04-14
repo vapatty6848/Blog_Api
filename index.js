4e15d6e7a62b6c24115844d270dbb7d1ef94db82
rafaelgelatti <rafael.gelatti@gmail.com>
2021-04-14 07:42:45 -0300

    fix: test to pass evaluator remote

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use(bodyParser.json());
 const UserControllers = require('./controllers/UserControllers');
 const LoginControllers = require('./controllers/LoginControllers');
 
-app.use('/user', UserControllers.routes);
-app.use('/login', LoginControllers.routes);
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use('/user', UserControllers.routes);
+app.use('/login', LoginControllers.routes);
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
