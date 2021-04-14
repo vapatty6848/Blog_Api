7708b85e454e0edae09e1b9716bef5cca89120f2
rafaelgelatti <rafael.gelatti@gmail.com>
2021-04-14 11:00:29 -0300

    feat: req9

--- controllers/PostControllers.js
+++ controllers/PostControllers.js
@@ -42,4 +42,49 @@ routes.get('/:id', verifyAuthorization, async (req, res) => {
   return res.status(200).json(post);
 });
 
+const checkPayload = (title, content, res) => {
+  if (!title) return res.status(400).json({ message: '"title" is required' });
+
+  if (!content) return res.status(400).json({ message: '"content" is required' });
+};
+
+const getPost = (id) => BlogPosts.findOne({
+  where: { id },
+  include: { model: Users, as: 'user' },
+});
+
+const getUser = (payload, id) => Users.findOne({
+  where: { email: payload.email },
+  attributes: { id },
+});
+
+const updatePost = (post, title, content, id) => BlogPosts.update(
+  { ...post, title, content },
+  { where: { id } },
+);
+
+routes.put('/:id', verifyAuthorization, async (req, res) => {
+  const { id } = req.params;
+  const { authorization: token } = req.headers;
+  const { title, content } = req.body;
+
+  checkPayload(title, content, res);
+
+  const post = await getPost(id);
+
+  const payload = validateToken(token);
+
+  const user = await getUser(payload, id);
+
+  const userId = Number(post.dataValues.userId);
+
+  if (Number(user.dataValues.id) === Number(post.dataValues.userId)) {
+    await updatePost(post, title, content, id);
+
+    return res.status(200).json({ title, content, userId });
+  }
+
+  return res.status(401).json({ message: 'Usuário não autorizado' });
+});
+
 module.exports = { routes };
