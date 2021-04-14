376920fb9f1a0c13cc09d6927abbbd02a9ec160c
Kesley Lahoz <namielahoz@gmail.com>
2021-04-14 15:11:56 -0300

    it doesn't work at all

--- migrations/20210409180558-create-user.js
+++ migrations/20210409180558-create-user.js
@@ -11,14 +11,6 @@ module.exports = {
       displayName: {
         type: Sequelize.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
       email: {
         allowNull: false,
         unique: true,
--- migrations/20210411154640-create-blog-post.js
+++ migrations/20210411154640-create-blog-post.js
@@ -11,14 +11,6 @@ module.exports = {
       title: {
         type: Sequelize.STRING
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
       content: {
         type: Sequelize.STRING
       },
