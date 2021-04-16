c259c9e325bde7859bbea3ab3f929041267c1610
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-04-16 12:46:16 -0300

    :rotating_light: fixing lint warnings

--- middlewares/postValidations.js
+++ middlewares/postValidations.js
@@ -2,7 +2,7 @@ const comebackResponse = require('../util/comebackResponse');
 const messages = require('../util/returnedMessages');
 
 const validateTitleAndContent = (req, res, next) => {
-  const { title, content} = req.body;
+  const { title, content } = req.body;
 
   if (!title) return comebackResponse(res, 400, messages.requiredTitle);
   if (!content) return comebackResponse(res, 400, messages.requiredContent);
