b45b16907eb142b1f02b972f0a23b1a9d7911194
Júlia Pôrto <juportob@gmail.com>
2021-04-16 15:07:52 -0300

    feat: add user

--- controller/postController.js
+++ controller/postController.js
@@ -58,21 +58,26 @@ postRouter.delete('/:id', verifyToken, async (req, res) => {
   }
 });
 
-postRouter.put('/:id', verifyToken, async (req, res) => {
+postRouter.put('/:id', registerPost, verifyToken, async (req, res) => {
   try {
+    const { title: titlePost, content: contentPost } = req.body;
+    console.log(titlePost, contentPost, 'title e contentttt');
     const { userData } = req;
     const tokenUserId = userData.id; // id do user
     const { id: idPost } = req.params; // id do post
-    const { title, content } = req.body
 
     const findIdPost = await BlogPost.findOne({ where: { id: idPost } });
+    if (findIdPost && findIdPost.userId !== tokenUserId) return res.status(401).json({ message: 'Usuário não autorizado' });
+
     if (findIdPost === null) return res.status(404).json({ message: 'Post não existe' });
 
-    const deletedPost = await BlogPost.update({ { title, content }, { where: { id: idPost, userId: tokenUserId } } });
+    const updatedPost = await BlogPost.update(
+      { title: titlePost, content: contentPost },
+      { where: { id: idPost } },
+    );
+    if (updatedPost === [0] || !updatedPost) return res.status(401).json({ message: 'Usuário não autorizado' });
 
-    if (deletedPost === 0 || !deletedPost) return res.status(401).json({ message: 'Usuário não autorizado' });
-
-    return res.status(204).end();
+    return res.status(200).json({ title: titlePost, content: contentPost, userId: tokenUserId });
   } catch (error) {
     return res.status(500).json({ message: 'Algo deu errado' });
   }
