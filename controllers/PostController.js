ec5c7dd42d73fc99e0ab62a576669c47e42ba142
Bruna Franciele <brunafranciele1902@gmail.com>
2021-04-16 15:08:35 -0300

    feat: add req 9

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -57,4 +57,25 @@ PostRouter.delete('/:id', verifyToken, async (req, res) => {
   }
 });
 
+PostRouter.put('/:id', registerPost, verifyToken, async (req, res) => {
+  try {
+    const { title: titlePost, content: contentPost } = req.body;
+    console.log(titlePost, contentPost, 'title e contentttt');
+    const { userData } = req;
+    const tokenUserId = userData.id; // id do user
+    const { id: idPost } = req.params; // id do post
+    const findIdPost = await BlogPost.findOne({ where: { id: idPost } });
+    if (findIdPost && findIdPost.userId !== tokenUserId) return res.status(401).json({ message: 'Usuário não autorizado' });
+    if (findIdPost === null) return res.status(404).json({ message: 'Post não existe' });
+    const updatedPost = await BlogPost.update(
+      { title: titlePost, content: contentPost },
+      { where: { id: idPost } },
+    );
+    if (updatedPost === [0] || !updatedPost) return res.status(401).json({ message: 'Usuário não autorizado' });
+    return res.status(200).json({ title: titlePost, content: contentPost, userId: tokenUserId });
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
 module.exports = PostRouter;
