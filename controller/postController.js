7bafce015ef23da01a8a7cf52ed674e0245b0c22
Júlia Pôrto <juportob@gmail.com>
2021-04-16 14:29:38 -0300

    feat: requirement 11

--- controller/postController.js
+++ controller/postController.js
@@ -40,4 +40,24 @@ postRouter.get('/:id', verifyToken, async (req, res) => {
   }
 });
 
+postRouter.delete('/:id', verifyToken, async (req, res) => {
+  try {
+    const { userData } = req;
+    const tokenUserId = userData.id; // id do user
+    console.log(tokenUserId, 'token idddd')
+
+    const { id: idPost } = req.params; // id do post
+
+    const findIdPost = await BlogPost.findOne({ where: { id: idPost } });
+    if (findIdPost === null) return res.status(404).json({ message: 'Post não existe' });
+
+    const deletedPost = await BlogPost.destroy({ where: { id: idPost, userId: tokenUserId } });
+    if (deletedPost === 0 || !deletedPost) return res.status(401).json({ message: 'Usuário não autorizado' });
+
+    return res.status(204).end();
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
 module.exports = postRouter;
