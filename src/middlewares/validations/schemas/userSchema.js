43ede4ed105875b172944f0b80d72bdc8892255e
paulo <paulocheloni@gmail.com>
2021-04-17 12:14:07 -0300

    refactor: eslint fixes

--- src/middlewares/validations/schemas/userSchema.js
+++ src/middlewares/validations/schemas/userSchema.js
@@ -2,14 +2,14 @@ const yup = require('yup');
 
 const userSchema = yup.object().shape({
   displayName: yup.string()
-    .min(8, "\"displayName\" length must be at least 8 characters long")
-    .required("\"displayName\" length must be at least 8 characters long"),
+    .min(8, '"displayName" length must be at least 8 characters long')
+    .required('"displayName" length must be at least 8 characters long'),
   email: yup.string()
-    .required("\"email\" is required")
-    .matches(/^[\w]+[@\w]+\.+\w/i, '\"email\" must be a valid email'),
+    .required('"email" is required')
+    .matches(/^[\w]+[@\w]+\.+\w/i, '"email" must be a valid email'),
   password: yup.string()
-    .min(6, '\"password\" length must be 6 characters long')
-    .required('\"password\" is required'),
+    .min(6, '"password" length must be 6 characters long')
+    .required('"password" is required'),
 
 });
 
