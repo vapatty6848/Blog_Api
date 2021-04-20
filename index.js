537a2556f75df9140b8cecbb927a71ffe8b79170
deko possas <andrepossaster@gmail.com>
2021-04-19 21:08:42 -0300

    trabalhei no index pra funcionar o user

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const UserRouter = require('./controllers/userController');
+require('dotenv').config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/user', UserRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
