94a664d395910a9b46747d8c424d77aab5acf71d
Alessandra <itsalematos@hotmail.com>
2021-04-21 11:10:59 -0400

    Feat: get by search term/query endpoint

--- controller/post/index.js
+++ controller/post/index.js
@@ -3,6 +3,7 @@ const getPosts = require('./GetPostsController');
 const getPostById = require('./GetPostByIdController');
 const editPostController = require('./EditPostController');
 const deletePostController = require('./DeletePostController');
+const getByTextController = require('./getByTextController');
 
 module.exports = {
   CreatePostController,
@@ -10,4 +11,5 @@ module.exports = {
   getPostById,
   editPostController,
   deletePostController,
+  getByTextController,
 };
--- routes/PostRoute.js
+++ routes/PostRoute.js
@@ -5,6 +5,7 @@ const {
   getPostById,
   editPostController,
   deletePostController,
+  getByTextController,
 } = require('../controller/post');
 const getPostByIdValidation = require('../middlewares/GetPostByIdValidate');
 const verifyToken = require('../middlewares/VerifyToken');
@@ -13,10 +14,10 @@ const checkUserValidation = require('../middlewares/checkUserValidation');
 
 const PostRouter = Router();
 
+PostRouter.get('/search', verifyToken, getByTextController);
 PostRouter.post('/', verifyToken, validatePostFields, CreatePostController);
 PostRouter.get('/', verifyToken, getPosts);
 PostRouter.get('/:id', verifyToken, getPostByIdValidation, getPostById);
 PostRouter.put('/:id', verifyToken, checkUserValidation, validatePostFields, editPostController);
 PostRouter.delete('/:id', verifyToken, checkUserValidation, getPostByIdValidation, deletePostController);
-
 module.exports = PostRouter;
