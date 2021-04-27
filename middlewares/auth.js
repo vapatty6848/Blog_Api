7a7dfdf91c38ce9c89093583dca162b828b2c505
deko possas <andrepossaster@gmail.com>
2021-04-27 18:07:18 -0300

    validate token done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,22 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'SARTOBOLAS';
 const config = { algorithm: 'Hs256', expiresIn: '7d' };
 
+const UNAUTHORIZED = 401;
+
 const createToken = (payload) => jwt.sign(payload.dataValues, SECRET, config);
 
-const varifyToken = (token) => jwt.verify(token, SECRET);
+const verifyToken = (token) => jwt.verify(token, SECRET);
+
+const validateToken = async(req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(UNAUTHORIZED).json({ message: 'Token não encontrado' });
+  }
+  try {
+    const payload = verifyToken(authorization, SECRET);
+    req.payload = payload;
+  } catch (error) {
+    return res.status(UNAUTHORIZED).json({ message: 'Token expirado ou inválido' });
+  }
+  return next();
+};
