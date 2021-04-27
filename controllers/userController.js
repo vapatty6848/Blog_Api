6ad96aba5108195ce87e521929e99c855a04cdb2
deko possas <andrepossaster@gmail.com>
2021-04-27 18:47:20 -0300

    testando a function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
 const { Router } = require('express');
 const models = require('../models');
 const auth = require('../middlewares/auth');
+const validateRegister = require('../middlewares/validateRegister');
 
 const UserRouter = new Router();
 
-UserRouter.post('/', async (req, res) => {
+UserRouter.post('/', validateRegister, async (req, res) => {
   const { email } = req.body;
   const uniqueEmail = await models.User.findOne({ where: { email } });
 
   if (uniqueEmail) return res.status(409).json({ message: 'Usuário já existe' });
 
   const user = await models.User.create(req.body);
-
   const tokenResponse = await auth.createToken(user);
 
   return res.status(201).json(tokenResponse);
