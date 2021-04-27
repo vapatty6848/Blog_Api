b9c725ee76e34cf1ca61d107dcf520ba19ec4059
deko possas <andrepossaster@gmail.com>
2021-04-27 18:25:02 -0300

    explorando refatoramento

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,23 +5,12 @@ const auth = require('../middlewares/auth');
 const UserRouter = new Router();
 
 UserRouter.post('/', async (req, res) => {
-  const {
-    displayName,
-    email,
-    password,
-    image,
-  } = req.body;
-
+  const { email } = req.body;
   const uniqueEmail = await models.User.findOne({ where: { email } });
 
   if (uniqueEmail) return res.status(409).json({ message: 'Usuário já existe' });
 
-  const user = await models.User.create({
-    displayName,
-    email,
-    password,
-    image,
-  });
+  const user = await models.User.create(req.body);
 
   const tokenResponse = await auth.createToken(user);
 
