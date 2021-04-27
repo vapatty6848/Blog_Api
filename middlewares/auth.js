31523f3d2a3b1b9dc81e809857e88f3e1eeaafd5
deko possas <andrepossaster@gmail.com>
2021-04-27 17:56:47 -0300

    verify e create token done

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,4 +3,6 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'SARTOBOLAS';
 const config = { algorithm: 'Hs256', expiresIn: '7d' };
 
+const createToken = (payload) => jwt.sign(payload.dataValues, SECRET, config);
 
+const varifyToken = (token) => jwt.verify(token, SECRET);
