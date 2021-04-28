8b01c2a4dae11cfdd9823a94e00949f8c680239a
deko possas <andrepossaster@gmail.com>
2021-04-28 10:48:13 -0300

    melhor function of the bahia

--- middlewares/validateRegister.js
+++ middlewares/validateRegister.js
@@ -10,28 +10,18 @@ const BAD_REQUEST = 400;
 const validateRegister = (req, res, next) => {
   const { displayName, email, password } = req.body;
   const message = {
-    displayName: '"displayName" length must be at least 8 characters long',
+    display: '"displayName" length must be at least 8 characters long',
     emailIsRequired: '"email" is required',
     emailisInvalid: '"email" must be a valid email',
     passIsRequired: '"password" is required',
     passIsInvalid: '"password" length must be 6 characters long',
   };
-  if (displayName.length < MIN_NAME_LENTGH) {
-    console.log(message.displayName);
-    return res.status(BAD_REQUEST).json(message.displayName);
-  } if (!email) {
-    console.log(message.emailIsRequired);
-    return res.status(BAD_REQUEST).json(message.emailIsRequired);
-  } if (!validateEmail(email)) {
-    console.log(message.emailisInvalid);
-    return res.status(BAD_REQUEST).json(message.emailisInvalid);
-  } if (!password) {
-    console.log(message.passIsRequired);
-    return res.status(BAD_REQUEST).json(message.passIsRequired);
-  } if (String(password).length < 6) {
-    console.log(message.passIsInvalid);
-    return res.status(BAD_REQUEST).json(message.passIsInvalid);
-  }
+  if (displayName.length < MIN_NAME_LENTGH) return res.status(BAD_REQUEST).json(message.display);
+  if (!email) return res.status(BAD_REQUEST).json(message.emailIsRequired);
+  if (!validateEmail(email)) return res.status(BAD_REQUEST).json(message.emailisInvalid);
+  if (!password) return res.status(BAD_REQUEST).json(message.passIsRequired);
+  if (String(password).length < 6) return res.status(BAD_REQUEST).json(message.passIsInvalid);
+
   return next();
 };
 
