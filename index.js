102c52d672861682b009ac2874923156ca2f4689
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-04-29 15:36:47 -0300

    2904-1536

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const { userRouter, loginRouter } = require('./controllers');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-// não remova esse endpoint, esta gambiarra é para o avaliador funcionar
+app.listen(PORT);
+console.log(`Server rodando a porta: ${PORT}`);
+
+// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
 
-const { userRouter, loginRouter } = require('./controllers');
-
 app.use('/user', userRouter);
 app.use('/login', loginRouter);
-
-app.listen(PORT);
-console.log(`Server rodando a porta: ${PORT}`);
