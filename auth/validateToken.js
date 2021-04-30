e0cd65465c7bcd446ffa1c9f506ac6b495c14e9e
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-04-29 21:51:18 -0300

    2904-2151

--- auth/validateToken.js
+++ auth/validateToken.js
@@ -4,7 +4,6 @@ const SECRET = 'my&7Ip$xk6PIsDL';
 
 const validateToken = async (req, res, next) => {
   const token = req.headers.authorization;
-  console.log(token)
 
   if (!token) {
     return res
