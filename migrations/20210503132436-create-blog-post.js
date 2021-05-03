26b6599776492e0c4ef21ad4aaf27c061440922b
Mario Duarte <mario.duarte@g.globo>
2021-05-03 10:27:30 -0300

    blogspots

---
+++ migrations/20210503132436-create-blog-post.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
