5a89975f09b5631562b739135c79871abd0258db
Mario Duarte <mario.duarte@g.globo>
2021-05-04 10:19:16 -0300

    Requisito 7

--- controllers/postController.js
+++ controllers/postController.js
@@ -8,13 +8,24 @@ const validationPost = require('../middlewares/validationPost');
 
 const router = express.Router();
 
+router.get('/', validateToken, (_req, res) => {
+  BlogPost.findAll()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    });
+});
+
 router.post('/', validateToken, validationPost, (req, res) => {
   const { title, content } = req.body;
   const { email } = req.user;
 
   User.findOne({ where: { email } })
     .then((user) => {
-      BlogPost.create({ title, content, userId: user.id })
+      BlogPost.create({ title, content, user })
         .then(() => res.status(201).json({ title, content, userId: user.id }))
         .catch((e) => {
           console.log(e.message);
