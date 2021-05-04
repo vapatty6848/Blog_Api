38fa2bd80df7c45944c889c53fc8a63ac963abe0
danilocouto1 <dansantosalfa@gmail.com>
2021-05-03 22:35:16 -0300

    teste 3

---
+++ migrations/20210405182031-create-blog-posts.js
@@ -0,0 +1,40 @@
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users', key: 'id',
+        }
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('BlogPosts'),
+};
