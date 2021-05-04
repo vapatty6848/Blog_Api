908743421a77048e776f92b6ddad704270f74a6b
Pedro Eduardo <pedroedu77@gmail.com>
2021-05-04 19:55:29 -0300

    Build routes and Reformulating service!

--- controller/userController.js
+++ controller/userController.js
@@ -12,6 +12,39 @@ const createUser = async (req, res, next) => {
   }
 };
 
+const getAllUsers = async (_req, res, next) => {
+  try {
+    const users = await user.getAll();
+
+    return res.status(StatusCodes.OK).json(users);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await user.getById(id);
+    return res.status(StatusCodes.OK).json(data);
+  } catch (err) {
+    return next(err);
+  }
+};
+
+const destroyUser = async (req, res, next) => {
+  try {
+    const { email } = req;
+    await user.destroyUser(email);
+    return res.status(204).end();
+  } catch (err) {
+    return next(err);
+  }
+};
+
 module.exports = {
   createUser,
+  getAllUsers,
+  getById,
+  destroyUser,
 };
--- service/userService.js
+++ service/userService.js
@@ -1,5 +1,7 @@
+const { StatusCodes } = require('http-status-codes');
+
 const { User } = require('../models');
-const schema = require('../schema');
+const CustomErr = require('../utils/customErr');
 const { generateToken } = require('../token');
 const userValidator = require('../validations/userValidation');
 
@@ -7,12 +9,25 @@ const createUser = async (displayName, email, password, image) => {
   userValidator(displayName, email, password);
 
   const user = await User.findOne({ where: { email } });
-  if (user) throw new Error(schema.user);
+  if (user) throw new CustomErr(StatusCodes.CONFLICT, 'Usuário já existe');
 
   const { dataValues: { id } } = await User.create({ displayName, email, password, image });
   return generateToken(email, id);
 };
 
+const getAll = async () => User.findAll();
+
+const getById = async (id) => {
+  const user = await User.findOne({ raw: true, where: { id } });
+  if (!user) throw new CustomErr(StatusCodes.NOT_FOUND, 'Usuário não existe');
+  return user;
+};
+
+const destroyUser = async (email) => User.destroy({ where: { email } });
+
 module.exports = {
   createUser,
+  getAll,
+  getById,
+  destroyUser,
 };
