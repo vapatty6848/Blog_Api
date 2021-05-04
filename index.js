5e8d58a0c4806a6e464251f6a34f4dcb7732e824
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-05-04 13:28:10 -0300

    Recomeçando o projeto!!

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const userRouter = require('./controllers/user');
-const loginRouter = require('./controllers/login');
-const postsRouter = require('./controllers/post');
-require('dotenv').config();
 
 const app = express();
 
-app.get('/', (_request, response) => {
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (request, response) => {
   response.send();
 });
-
-app.use(bodyParser.json());
-
-app.use('/user', userRouter);
-app.use('/login', loginRouter);
-app.use('/post', postsRouter);
-
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
