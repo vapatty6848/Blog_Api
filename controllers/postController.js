4d10b86e60ff25b80fa945b711de3035e3c752a2
LughWalle <civalfran1@gmail.com>
2021-05-10 17:24:22 -0300

    req 8 completo

--- controllers/postController.js
+++ controllers/postController.js
@@ -17,7 +17,18 @@ router.post('/', verifyToken, validatePost, async (req, res) => {
 
 router.get('/', verifyToken, async (_req, res) => {
   await BlogPost.findAll({ include: { model: User, as: 'user', attributes: { exclude: 'password' } } })
-    .then((answer) => res.status(200).json(answer));
+    .then((post) => res.status(200).json(post));
+});
+
+router.get('/:id', verifyToken, async (req, res) => {
+  const { id } = req.params;
+  await BlogPost.findByPk(id, { include: { model: User, as: 'user', attributes: { exclude: 'password' } } })
+    .then((post) => {
+      if (!post) {
+        return res.status(StatusCodes.NOT_FOUND).json({ message: 'Post não existe' });
+      }
+      return res.status(200).json(post);
+    });
 });
 
 module.exports = router;
